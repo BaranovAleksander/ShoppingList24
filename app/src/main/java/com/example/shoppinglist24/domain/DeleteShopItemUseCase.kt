184db6846418373eda09b8f58c5ee75758a57b77
Baranov Aleksander <baranovalexander1992@gmail.com>
2023-10-04 08:39:43 +0300

    Завешрил бизнес логику в Домаин слое

--- app/src/main/java/com/example/shoppinglist24/domain/DeleteShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglist24/domain/DeleteShopItemUseCase.kt
@@ -1,7 +1,7 @@
 package com.example.shoppinglist24.domain
 
-class DeleteShopItemUseCase {
+class DeleteShopItemUseCase(private val shopLIstRepository: ShopLIstRepository) {
     fun deleteShopItem(shopItem: ShopItem) {
-
+        shopLIstRepository.deleteShopItem(shopItem)
     }
 }
--- app/src/main/java/com/example/shoppinglist24/domain/EditShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglist24/domain/EditShopItemUseCase.kt
@@ -1,7 +1,8 @@
 package com.example.shoppinglist24.domain
 
-class EditShopItemUseCase {
-    fun editShopItem() {
-        TODO()
+class EditShopItemUseCase(private val shopLIstRepository: ShopLIstRepository) {
+    fun editShopItem(shopItem: ShopItem) {
+        shopLIstRepository.editShopItem(shopItem)
+
     }
 }
