074406836a56b9098c0927cb150cbbccf4209117
Baranov Aleksander <baranovalexander1992@gmail.com>
2023-10-09 09:06:14 +0300

    Завешрил подготовку презинтейшн слоя

--- app/src/main/java/com/example/shoppinglist24/domain/ShopLIstRepository.kt
+++ app/src/main/java/com/example/shoppinglist24/domain/ShopLIstRepository.kt
@@ -1,10 +1,12 @@
 package com.example.shoppinglist24.domain
 
+import androidx.lifecycle.LiveData
+
 interface ShopLIstRepository {
     fun addShopItem(shopItem: ShopItem)
     fun deleteShopItem(shopItem: ShopItem)
     fun editShopItem(shopItem: ShopItem)
     fun getShopItem(shopItem: Int):ShopItem
-    fun getShopList(): List<ShopItem>
+    fun getShopList(): LiveData<List<ShopItem>>
 
 }
