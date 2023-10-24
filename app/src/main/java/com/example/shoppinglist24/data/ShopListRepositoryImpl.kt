dafa53b3ebb69fef7ff10be0a129f7178b15f3bd
Baranov Aleksander <baranovalexander1992@gmail.com>
2023-10-24 20:44:04 +0300

    Добавил макеты для отображения списков

--- app/src/main/java/com/example/shoppinglist24/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist24/data/ShopListRepositoryImpl.kt
@@ -7,17 +7,16 @@ import com.example.shoppinglist24.domain.ShopLIstRepository
 
 object ShopListRepositoryImpl : ShopLIstRepository {
     private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id) })
 
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 until 10) {
+        for (i in 0 until 10000) {
             val item = ShopItem("Name $i", i, true)
             addShopItem(item)
         }
     }
-
 
     override fun addShopItem(shopItem: ShopItem) {
         if (shopItem.id == ShopItem.UNDEFINED_ID) {
@@ -30,7 +29,6 @@ object ShopListRepositoryImpl : ShopLIstRepository {
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
         updateList()
-
     }
 
     override fun editShopItem(shopItem: ShopItem) {
@@ -48,6 +46,7 @@ object ShopListRepositoryImpl : ShopLIstRepository {
     override fun getShopList(): LiveData<List<ShopItem>> {
         return shopListLD
     }
+
     private fun updateList() {
         shopListLD.value = shopList.toList()
     }
